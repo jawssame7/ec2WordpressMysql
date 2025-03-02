413ef4fc51edb7570c5a5ca2822aec61e8258291
Hirofumi Sameshima <h.sameshima@wiz-net.jp>
2025-03-02 12:32:20 +0900

    Initial commit

---
+++ test/ec2_wordpress_mysql.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ec2WordpressMysql from '../lib/ec2_wordpress_mysql-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ec2_wordpress_mysql-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ec2WordpressMysql.Ec2WordpressMysqlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
